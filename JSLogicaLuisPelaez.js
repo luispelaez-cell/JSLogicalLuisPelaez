5ecd57eeb962f222f9f366d7cf315cd7ca6e0138
luisp <luis.pelaez@institutvidreres.cat>
2025-11-14 12:16:23 +0100

    Ex2

--- JSLogicaLuisPelaez.js
+++ JSLogicaLuisPelaez.js
@@ -5,3 +5,8 @@ if (num % 3 === 0 || num % 7 === 0) {
 } else {
     console.log(`${num} no és múltiple de 3 ni de 7`);
 }
+let x = 12;
+let y = 55;
+
+x >= 10 || x <= 50 ? console.log(x, "esta en el rang"): null
+y >= 10 || y <= 50 ? console.log(x, "esta en el rang"): null
