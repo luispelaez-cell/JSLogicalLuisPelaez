f2b9a49b7f18d3cfdc8f9790084b73d40746a2e5
luisp <luis.pelaez@institutvidreres.cat>
2025-11-17 11:01:17 +0100

    Ex3

--- JSLogicaLuisPelaez.js
+++ JSLogicaLuisPelaez.js
@@ -10,3 +10,16 @@ let y = 55;
 
 x >= 10 || x <= 50 ? console.log(x, "esta en el rang"): null
 y >= 10 || y <= 50 ? console.log(x, "esta en el rang"): null
+
+let xx = 5.99;
+if (xx >= 10){
+    console.log("Excel·lent")
+} else if (xx >= 8.99) {
+    console.log("Notable")
+} else if(xx >= 6.99){
+    console.log("Bé")
+} else if(xx >= 5.99){
+    console.log("Suficient")
+}else if (xx >= 4.99) {
+    console.log("Suspes")
+}
